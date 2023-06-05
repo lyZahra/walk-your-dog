f6b37d5905e63cedae6ebf832078ac2998be8ef3
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-05 23:37:46 +0200

    changing file type

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,29 +20,29 @@ nurra = User.create!(email: "example.example@gmail.com", password: "secret", nam
 bella_file = URI.open("https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg")
 bella = Pet.new(name: "Bella", availability: true, breed: "poodle", description: "cuddling and love chicken")
 bella.user = juliette
-bella.photo.attach(io: bella_file, filename: "bella.jpg", content_type: "image/jpg")
+bella.photo.attach(io: bella_file, filename: "bella.png", content_type: "image/png")
 bella.save
 
 max_file = URI.open("https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg")
 max = Pet.new(name: "Max", availability: true, breed: "labrador", description: "super loyal, don't like to run")
 max.user = tom
-max.photo.attach(io: max_file, filename: "max.jpg", content_type: "image/jpg")
+max.photo.attach(io: max_file, filename: "max.png", content_type: "image/png")
 max.save
 
 luna_file = URI.open("https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg")
 luna = Pet.new(name: "Luna", availability: false, breed: "golden retriever", description: "yound and super athletic")
 luna.user = chris
-luna.photo.attach(io: luna_file, filename: "luna.jpg", content_type: "image/jpg")
+luna.photo.attach(io: luna_file, filename: "luna.png", content_type: "image/png")
 luna.save
 
 cooper_file = URI.open("https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg")
 cooper = Pet.new(name: "Cooper", availability: true, breed: "bulldog", description: "very social ! Cooper loves others dogs")
 cooper.user = safia
-cooper.photo.attach(io: cooper_file, filename: "cooper.jpg", content_type: "image/jpg")
+cooper.photo.attach(io: cooper_file, filename: "cooper.png", content_type: "image/png")
 cooper.save
 
 daisy_file = URI.open("https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg")
 daisy = Pet.new(name: "Daisy", availability: false, breed: "german sheperd", description: "Daisy is super kind, she loves walking under rain")
 daisy.user = nurra
-daisy.photo.attach(io: daisy_file, filename: "daisy.jpg", content_type: "image/jpg")
+daisy.photo.attach(io: daisy_file, filename: "daisy.png", content_type: "image/png")
 daisy.save
