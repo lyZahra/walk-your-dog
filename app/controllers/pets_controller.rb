12291837e697c97635e990df1e3477011f8d5abc
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-05 18:28:18 +0200

    updating pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -45,6 +45,6 @@ class PetsController < ApplicationController
   end
 
   def pet_params
-    params.require(:pet).permit(:name, :availability, :breeed, :description)
+    params.require(:pet).permit(:name, :availability, :breed, :description)
   end
 end
