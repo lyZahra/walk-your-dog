ae7c8d572d07e167c8701db4f8f49cbea474abf5
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-05 13:55:41 +0200

    adding pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,24 +1,48 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
 
   def index
-     @pets = Pet.all
+    @pets = Pet.all
   end
 
   def show
-    @pet = Pet.find(params[:id])
+    # uses :set_pet
   end
 
   def new
-    @restaurant = Restaurant.new
+    @pet = Pet.new
   end
 
   def create
     @pet = Pet.new(params[:pet])
-    @pet.save
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # uses :set_pet
+  end
+
+  def update
+    # uses :set_pet
+    @pet.update(pet_params)
     redirect_to pet_path(@pet)
   end
 
+  def destroy
+    # uses :set_pet
+    @pet.destroy
+    redirect_to pets_path
+  end
+
   private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 
   def pet_params
     params.require(:pet).permit(:name, :availability, :breeed, :description)
