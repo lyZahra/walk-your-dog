fcb56be98079148bcc1246325f89677224f794ed
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-05 12:47:10 +0200

    adding pet model

---
+++ app/models/pet.rb
@@ -0,0 +1,3 @@
+class Pet < ApplicationRecord
+  belongs_to :user_id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "pets#home"
+  resources :pets, only: ["index", "new", "create"]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
