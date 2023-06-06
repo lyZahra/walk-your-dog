cd2680c3cfa8cc19db25a5a9dbfc9b1a42b22041
Juliette Guillaume <glm.juliette@gmail.com>
2023-06-06 15:04:14 +0200

    dashboard styling show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    # params[:id] == current_user.id unless params[:id]
-    @user = current_user.booking
+    @user = User.all
     # @user_subscriptions = UserSubscription.find_active_subscriptions_by_user(@user)
   end
 
