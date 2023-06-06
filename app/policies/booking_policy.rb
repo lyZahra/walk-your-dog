496130cb12c34740dc3dc80d093d38c4b836bc18
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-06 16:16:49 +0200

    saving

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,23 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def show?
+      true
+    end
+
+    def new?
+      create?
+    end
+
+    def create?
+      true
+    end
+
+    def update?
+      record.user == user
+    end
+  end
+end
