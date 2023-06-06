03c88168ddac25e36c354ade3cfb6cba190405d4
Juliette Guillaume <glm.juliette@gmail.com>
2023-06-06 15:49:42 +0200

    dashboard init style show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    @user = User.all
+    @user = current_user.bookings
     # @user_subscriptions = UserSubscription.find_active_subscriptions_by_user(@user)
   end
 
