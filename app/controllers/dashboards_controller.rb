7eaa6b6c2fb41e02524d0c72f0c51a7a8337a136
Juliette Guillaume <glm.juliette@gmail.com>
2023-06-06 16:13:16 +0200

    Dashboard updates

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
   def show
-    @user = current_user.bookings
+    @user = current_user
     # @user_subscriptions = UserSubscription.find_active_subscriptions_by_user(@user)
+    @bookings = @user.bookings
+    authorize @bookings
   end
-
 end
