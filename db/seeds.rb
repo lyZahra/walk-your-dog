c95ca5348d9521e43a61d343f76bab7777646b07
Nurra Barry <billing@carbon-saver.com>
2023-06-07 15:58:28 +0200

    new dogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,31 +17,31 @@ chris = User.create!(email: "guywholovesdogs@gmail.com", password: "secret", nam
 safia = User.create!(email: "example@gmail.com", password: "secret", name: "Safia")
 nurra = User.create!(email: "example.example@gmail.com", password: "secret", name: "Nurra")
 
-bella_file = URI.open("https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+bella_file = URI.open("https://source.unsplash.com/random/?dog")
 bella = Pet.new(name: "Bella", availability: true, breed: "poodle", description: "cuddling and love chicken")
 bella.user = juliette
 bella.photo.attach(io: bella_file, filename: "bella.png", content_type: "image/png")
 bella.save
 
-max_file = URI.open("https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+max_file = URI.open("https://source.unsplash.com/random/?dog")
 max = Pet.new(name: "Max", availability: true, breed: "labrador", description: "super loyal, don't like to run")
 max.user = tom
 max.photo.attach(io: max_file, filename: "max.png", content_type: "image/png")
 max.save
 
-luna_file = URI.open("https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+luna_file = URI.open("https://source.unsplash.com/random/?dog")
 luna = Pet.new(name: "Luna", availability: false, breed: "golden retriever", description: "yound and super athletic")
 luna.user = chris
 luna.photo.attach(io: luna_file, filename: "luna.png", content_type: "image/png")
 luna.save
 
-cooper_file = URI.open("https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+cooper_file = URI.open("https://source.unsplash.com/random/?dog")
 cooper = Pet.new(name: "Cooper", availability: true, breed: "bulldog", description: "very social ! Cooper loves others dogs")
 cooper.user = safia
 cooper.photo.attach(io: cooper_file, filename: "cooper.png", content_type: "image/png")
 cooper.save
 
-daisy_file = URI.open("https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+daisy_file = URI.open("https://source.unsplash.com/random/?dog")
 daisy = Pet.new(name: "Daisy", availability: false, breed: "german sheperd", description: "Daisy is super kind, she loves walking under rain")
 daisy.user = nurra
 daisy.photo.attach(io: daisy_file, filename: "daisy.png", content_type: "image/png")
