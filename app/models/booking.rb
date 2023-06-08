414be8a00211646c575ac432f38a47e956b559dc
Juliette Guillaume <glm.juliette@gmail.com>
2023-06-08 14:42:46 +0200

    saved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
-<<<<<<< HEAD
   has_many :reviews, dependent: :destroy
-=======
-  has_one :review, dependent: :destroy
-  # STATUSES = %i[pending accepted denied completed].map(&:to_s).freeze
-  # validates :quote_requestor, inclusion: { in: Booking::STATUSES }
->>>>>>> master
 end
